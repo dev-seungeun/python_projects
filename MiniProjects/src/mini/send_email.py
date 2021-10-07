7ece50ea011fd34d757e6ba4b8953f046349d35c
seungeun.baek <dev.seungeun@gmail.com>
2021-10-08 00:17:20 +0900

    이메일전송 모듈화 하기

--- MiniProjects/src/mini/send_email.py
+++ MiniProjects/src/mini/send_email.py
@@ -8,77 +8,169 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-from_addr = formataddr(('Google Test_Seungeun', 'test.seungeun@gmail.com'))
-to_addr = formataddr(('Google Dev_Seungeun', 'dev.seungeun@gmail.com'))
 
-session = None
-try:
-    # SMTP 세션 생성, 로그인
-    session = smtplib.SMTP('smtp.gmail.com', 587)
-    session.set_debuglevel(True)  # 상세 로그 출력
+class SendMail(object):
+    ENCODING = 'utf-8'
 
-    # SMTP 계정 인증 설정
-    session.ehlo()
-    session .starttls()
-    session .login('test.seungeun@gmail.com', 'hakaituvzatfwnwl')
+    host = None
+    port = None
+    from_addr = None
+    to_addrs = None
+    message = None
 
-    # 메일 컨텐츠 설정
-    message = MIMEMultipart("alternative")
+    debug = False
+    _isLogin = False
+    _existAttachment = False
 
-    # 메일 송/수신 옵션 설정
-    title = '제목이다'
-    message.set_charset('utf-')
-    message['From'] = from_addr
-    message['To'] = to_addr
-    message['Subject'] = title
+    def __init__(self, address: tuple, from_addr: str, to_addrs: list, debug=False, **kwargs):
+        self.host, self.port = address
+        self.from_addr = from_addr
+        self.to_addrs = to_addrs
 
-    # 메일 컨텐츠 - 내용
-    body = '''
-    <h2>안녕?</h2>
-    <h4>난 승은이얌</h4>
-    <h4>파이썬을 공부하고 있찌!</h4>
-    '''
-    bodyPart = MIMEText(body, 'html', 'utf-8')
-    message.attach(bodyPart)
+        # 인증계정
+        if kwargs.get("username") is not None:
+            self.setAuth(
+                kwargs.get("username"),
+                kwargs.get("password")
+            )
 
-    # 메일 컨텐츠 - 첨부파일
-    attachments = [
-        os.path.join(os.getcwd(), 'storage', 'example.txt')
-    ]
+        # 디버그 모드
+        self.debug = debug is not False
 
-    for attachment in attachments:
-        attach_binary = MIMEBase('application', 'octect-stream')
+        # 첨부파일로 전송 불가능한 확장자
+        self.blocked_extensions = (
+            ".ade", ".adp", ".apk", ".appx", ".appxbundle", ".bat"
+            , ".cab", ".chm", ".cmd", ".com", ".cpl", ".dll", ".dmg"
+            , ".exe", ".hta", ".ins", ".isp", ".iso", ".jar", ".js"
+            , ".jse", ".lib", ".lnk", ".mde", ".msc", ".msi", ".msix"
+            , ".msixbundle", ".msp", ".mst", ".nsh", ".pif", ".ps1"
+            , ".scr", ".sct", ".shb", ".sys", ".vb", ".vbe", ".vbs"
+            , ".vxd", ".wsc", ".wsf", ".wsh"
+        )
+
+    def setAuth(self, username, password):
+        self._isLogin = True
+        self.username = username
+        self.password = password
+
+    def setMail(self, subject, body, body_type='plain', attachments=None):
+        """
+        Content-Type:
+            - multipart/alternative: 평문 텍스트와 HTML과 같이 다른 포맷을 함께 보낸 메시지
+            - multipart/mixed: 첨부파일이 포함된 메시지
+
+        REF:
+            - https://ko.wikipedia.org/wiki/MIME#Content-Type
+        """
+        # 첨부파일 여부
+        self._existAttachments = attachments is not None
+        self.content_sub_type = "mixed" if self._existAttachments else "alternative"
+
+        # 메일 콘텐츠 설정
+        self.message = MIMEMultipart(self.content_sub_type)
+
+        # 받는 사람 주소 설정. [( 이름 <이메일> )]
+        self.FROM_ADDR_FMT = formataddr(self.from_addr)
+        self.TO_ADDRS_FMT = ",".join([formataddr(addr) for addr in self.to_addrs])
+
+        # 메일 헤더 설정
+        self.message.set_charset(self.ENCODING)
+        self.message['From'] = self.FROM_ADDR_FMT
+        self.message['To'] = self.TO_ADDRS_FMT
+        self.message['Subject'] = subject
+
+        # 메일 콘텐츠 - 내용
+        self.message.attach(MIMEText(body, body_type, self.ENCODING))
+
+        # 메일 콘텐츠 - 첨부파일
+        if self._existAttachments:
+            self._attach_files(attachments)
+
+        return self
+
+    def _attach_files(self, attachments):
+        """
+        Content-disposition:
+            - 파일명 지정
+        MIME type:
+            - application/octect-stream: Binary Data
+        REF:
+            - https://www.freeformatter.com/mime-types-list.html
+        """
+
+        for attachment in attachments:
+            attach_binary = MIMEBase("application", "octect-stream")
+            try:
+                binary = open(attachment, "rb").read()  # read file to bytes
+
+                attach_binary.set_payload(binary)
+                encoders.encode_base64(attach_binary)  # Content-Transfer-Encoding: base64
+
+                filename = os.path.basename(attachment)
+                attach_binary.add_header("Content-Disposition", 'attachment', filename=(self.ENCODING, '', filename))
+
+                self.message.attach(attach_binary)
+            except Exception as e:
+                print(e)
+
+    def send(self):
+        session = None
         try:
-            binary = open(attachment, 'rb').read()  # read file to bytes
+            # 메일 세션 생성
+            session = smtplib.SMTP(self.host, self.port)
+            session.set_debuglevel(self.debug)
+            session.ehlo()
 
-            attach_binary.set_payload(binary)
-            encoders.encode_base64(attach_binary)  # Content-Transfer-Encoding: base64
+            # SMTP 서버 계정 인증
+            if self._isLogin:
+                session.starttls()
+                session.login(self.username, self.password)
 
-            filename = os.path.basename(attachment)
-            attach_binary.add_header('Content-Disposition', 'attachment', filename=('utf-8', '', filename))
+            # 메일 발송
+            session.sendmail(self.FROM_ADDR_FMT, self.TO_ADDRS_FMT, self.message.as_string())
+            print('Successfully sent the mail!!!')
 
-            message.attach(attach_binary)
         except Exception as e:
             print(e)
-
-    # 메일 발송
-    session.sendmail(from_addr, to_addr, message.as_string())
-
-    print('Successfully sent the mail!!!')
-
-except Exception as e:
-    print(e)
-
-finally:
-    if session is not None:
-        session.quit()
+        finally:
+            if session is not None:
+                session.quit()
 
 
-# 파일첨부 (파일 미첨부시 생략가능)
-# attachment = open('파일명', 'rb')
-# part = MIMEBase('application', 'octet-stream')
-# part.set_payload((attachment).read())
-# encoders.encode_base64(part)
-# part.add_header('Content-Disposition', "attachment; filename= " + filename)
-# msg.attach(part)
+# SMTP 서버 정보
+smtp_address = ('smtp.gmail.com', 587)  # SMTP 서버 호스트, 포트
+
+# SMTP 계정 정보
+username = 'test.seungeun@gmail.com'  # Gmail 계정
+password = 'hakaituvzatfwnwl'  # Gmail 앱 비밀번호
+
+# 메일 송/수신자 정보
+from_addr = ('Google Test_Seungeun', 'test.seungeun@gmail.com')  # 보내는 사람 주소. (이름, 이메일)
+to_addrs = [  # 받는 사람 주소. (이름, 이메일)
+    ('Google Dev_Seungeun', 'dev.seungeun@gmail.com'),
+    ('Naver bse14', 'bse14@naver.com')
+]
+
+# 메일 송/수신 옵션 설정
+subject = '제목이다'
+
+# 메일 컨텐츠 - 내용
+body = '''
+<h2>안녕?</h2>
+<h4>난 승은이얌</h4>
+<h4>파이썬을 공부하고 있찌!</h4>
+<h4>이메일 보내는 모듈을 만들었오</h4>
+'''
+
+# 메일 컨텐츠 - 첨부파일
+attachments = [
+    os.path.join(os.getcwd(), 'storage', 'example.txt')
+]
+
+if __name__ == '__main__':
+    mail = SendMail(smtp_address, from_addr, to_addrs, debug=False)
+
+    mail.setAuth(username, password)
+    mail.setMail(subject, body, 'html', attachments=attachments)
+    mail.send()
 
