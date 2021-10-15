07817fcfc2fceef029be6cfed04db253ad0a3f26
seungeun.baek <dev.seungeun@gmail.com>
2021-10-15 23:59:39 +0900

    sys.stdin list map

--- PythonStart/src/study2/pygorithm.py
+++ PythonStart/src/study2/pygorithm.py
@@ -1,12 +1,14 @@
 import sys
 
-"""
-input() 함수와 기능이 똑같다. 
-여기서 알아 두면 좋은 것은
-sys.stdin은 file 객체와 사용 방법이 같다. 
-단지 입력을 인터프리터 내에서 받냐, 파일에서 받냐가 다를 뿐이다.
-"""
+# sys.stdin.readline()
+
 n1 = int(sys.stdin.readline())
 print("sys.stdin :", n1)
 n2 = input()
 print("input :", n2)
+
+arr = sys.stdin.readline().split()
+print("sys.stdin.readline().split() :", arr)
+arr = list(map(int, sys.stdin.readline().split()))
+print("sys.stdin.readline().split() -> map int :", arr)
+
