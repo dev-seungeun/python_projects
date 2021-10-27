9a292d4c9b0b3e07909e6fafa65e2269179f5c71
seungeun.baek <dev.seungeun@gmail.com>
2021-10-27 21:32:58 +0900

    python

--- PythonStart/tests/package1/array.py
+++ PythonStart/tests/package1/array.py
@@ -2,5 +2,5 @@ class Array(object):
     def sum(self, size, array_string):
         numbers = [int(number) for number in array_string.split(' ')]
         if size != len(numbers):
-            raise Exception('array size is not matched')
+            raise Exception('array size is not matched!')
         return sum(numbers)
